2458fe3168ca7e70e635c77140039ba1d2bd54e8
amir2501 <113626369+amir2501@users.noreply.github.com>
2024-05-17 11:04:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  let width = screen.width;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("img").style.width = "80vw";
+  } else {
+    document.getElementById("img").style.width = "100vw";
+    if (width > 800) {
+      document.getElementById("absolute").style.position = "relative";
+      document.getElementById("absolute").style.left = "0px";
+    }
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+document.querySelector("nav input").onclick = function () {
+  if (this.checked) this.setAttribute("aria-expanded", true);
+  else this.setAttribute("aria-expanded", false);
+};
+
+// loading screen
+
+document.onreadystatechange = function () {
+  var state = document.readyState;
+  document.getElementById("nav").classList.add("hidden");
+  if (state == "complete") {
+    setTimeout(function () {
+      document.getElementById("interactive");
+      document.getElementById("loading").style.display = "none";
+      document.getElementById("nav").classList.remove("hidden");
+    }, 1000);
+  }
+};
